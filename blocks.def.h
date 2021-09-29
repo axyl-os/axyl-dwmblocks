a6a34cdc340c2023b35bcf94d61add575b0d4900
Kungger-git <matthewriel63@gmail.com>
2021-09-29 09:00:55 +0800

    updated dwmblocks config

--- blocks.def.h
+++ blocks.def.h
@@ -3,19 +3,19 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 /*    {"  ", "uname -r", 360, 0}, */
 
-    {" ﬙ ", "./.config/dwmblocks/scripts/cpu",        3,        0},
+    {" ﬙ ", "/opt/axyl-dwmblocks/scripts/cpu",        3,        0},
 
-	{" ", "./.config/dwmblocks/scripts/ram",	      3,		0},
+	{" ", "/opt/axyl-dwmblocks/scripts/ram",	      3,		0},
 
-    {" ", "./.config/dwmblocks/scripts/storage",     3,        0},
+    {" ", "/opt/axyl-dwmblocks/scripts/storage",     3,        0},
     
-    {" ", "./.config/dwmblocks/scripts/packages",    5,        0},
+    {" ", "/opt/axyl-dwmblocks/scripts/packages",    5,        0},
 
-	{" ", "./.config/dwmblocks/scripts/date",		360,		0},
+	{" ", "/opt/axyl-dwmblocks/scripts/date",		360,		0},
 
-	{" ", "./.config/dwmblocks/scripts/time",		  1,		0},
+	{" ", "/opt/axyl-dwmblocks/scripts/time",		  1,		0},
     
-    {"", "./.config/dwmblocks/scripts/uptime",       30,        0},
+    {"", "/opt/axyl-dwmblocks/scripts/uptime",       30,        0},
 
 };
 
