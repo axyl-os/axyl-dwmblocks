918873ff79bb5d7ca1ee9d11ce00db70cb32ff73
Kungger-git <matthewriel63@gmail.com>
2021-10-19 16:11:51 +0800

    modified siduck's dwmblocks configuration, and added some more scripts

--- opt/axyl-dwmblocks/blocks.def.h
+++ opt/axyl-dwmblocks/blocks.def.h
@@ -1,24 +1,13 @@
-
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-/*    {"  ", "uname -r", 360, 0}, */
-
-    {" ﬙ ", "/opt/axyl-dwmblocks/scripts/cpu",        3,        0},
-
-	{" ", "/opt/axyl-dwmblocks/scripts/ram",	      3,		0},
-
-    {" ", "/opt/axyl-dwmblocks/scripts/storage",     3,        0},
-    
-    {" ", "/opt/axyl-dwmblocks/scripts/packages",    5,        0},
-
-	{" ", "/opt/axyl-dwmblocks/scripts/date",		360,		0},
-
-	{" ", "/opt/axyl-dwmblocks/scripts/time",		  1,		0},
-    
-    {"", "/opt/axyl-dwmblocks/scripts/uptime",       30,        0},
-
+	// {"Icon", "Command", "update interval", "update signal"}
+	//{"", "/opt/axyl-dwmblocks/scripts/packages",30,0},
+	{"", "/opt/axyl-dwmblocks/scripts/battery",30,0},
+	{"", "/opt/axyl-dwmblocks/scripts/volume",1,0},
+	{"", "/opt/axyl-dwmblocks/scripts/brightness",1,0},
+	{"", "/opt/axyl-dwmblocks/scripts/cpu",3,0},
+	{"", "/opt/axyl-dwmblocks/scripts/memory",3,0},
+	{"", "/opt/axyl-dwmblocks/scripts/storage",3,0},
+	{"", "/opt/axyl-dwmblocks/scripts/clock",10,0},
+	{"", "/opt/axyl-dwmblocks/scripts/uptime",30,0},
 };
-
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  ";
-static unsigned int delimLen = 5;
+static char delim = ' ';
