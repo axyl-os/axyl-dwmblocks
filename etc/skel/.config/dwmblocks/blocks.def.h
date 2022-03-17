e4fdf00e4735a0bfaa7c53262d548e8ef23f3bda
Kungger-git <matthewriel63@gmail.com>
2022-03-17 10:37:41 +0800

    [Fix]: Returned `dwmblocks` configuration

---
+++ etc/skel/.config/dwmblocks/blocks.def.h
@@ -0,0 +1,13 @@
+static const Block blocks[] = {
+	// {"Icon", "Command", "update interval", "update signal"}
+	//{"", "/usr/local/bin/dwmblocks-scripts/packages",30,0},
+	{"", "/usr/local/bin/dwmblocks-scripts/volume",1,0},
+	{"", "/usr/local/bin/dwmblocks-scripts/brightness",1,0},
+	{"", "/usr/local/bin/dwmblocks-scripts/battery",30,0},
+	{"", "/usr/local/bin/dwmblocks-scripts/cpu",3,0},
+	{"", "/usr/local/bin/dwmblocks-scripts/memory",3,0},
+	{"", "/usr/local/bin/dwmblocks-scripts/storage",3,0},
+	{"", "/usr/local/bin/dwmblocks-scripts/clock",10,0},
+	{"", "/usr/local/bin/dwmblocks-scripts/uptime",30,0},
+};
+static char delim = ' ';
